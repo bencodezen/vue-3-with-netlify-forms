504f63aa9b92766875e025e5dbcd1e1b2ba80068
Ben Hong <ben@bencodezen.io>
2022-01-20 10:47:03 -0500

    refactor: use encode to include reactive data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,14 +13,20 @@ const data = reactive({
 
 const elFeedbackForm = ref(null)
 
-const submitForm = () => {
-  const formData = new FormData(elFeedbackForm.value)
-  const fetchBody = new URLSearchParams(formData).toString()
+const encode = data => {
+  return Object.keys(data)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&')
+}
 
+const submitForm = () => {
   fetch('/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: fetchBody
+    body: encode({
+      'form-name': 'feedback',
+      ...data
+    })
   })
     .then(() => console.log('Form successfully submitted'))
     .catch(error => alert(error))
